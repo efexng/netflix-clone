f629200d9f889559ea34278d893e7b4e22555357
efexng <efeapoki2020@gmail.com>
2024-05-07 12:32:16 +0100

    chnages

---
+++ src/Homescreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homescreen.css'
+
+function Homescreen() {
+  return (
+    <div className='homescreen'>
+      
+    </div>
+  )
+}
+
+export default Homescreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB78iwAtIEHafTcXhBg1wfPHJ6j-wHEwDs",
+    authDomain: "netflix-clone-b78bf.firebaseapp.com",
+    projectId: "netflix-clone-b78bf",
+    storageBucket: "netflix-clone-b78bf.appspot.com",
+    messagingSenderId: "717757026269",
+    appId: "1:717757026269:web:27dd445e101aeb65873034"
+  };
