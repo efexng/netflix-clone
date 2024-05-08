7ab2bb83e2fe6cda8cf7be63a7a2d92ea17ee58d
efexng <efeapoki2020@gmail.com>
2024-05-08 14:25:36 +0100

    Remove src/firebase.js from tracking

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
 import './App.css';
-import Homescreen from './Homescreen';
+import Homescreen from './screens/Homescreen';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, login, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
+import LoginScreen from './screens/LoginScreen';
+
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid, 
+          email: userAuth.email
+        }))
+      } else  {
+        dispatch(logout())
+      }
+    });
+  
+    return () => unsubscribe();
+  }, [dispatch]); // Include dispatch in the dependency array
+  
+
   return (
     <div className="App">
-      <Homescreen />
+
+      <Router>
+      {!user ? (
+        <LoginScreen />
+      ) : (
+        <Switch>
+        <Route exact path="/profile">
+         <ProfileScreen />
+        </Route>
+          <Route exact path="/">
+          <Homescreen />
+          </Route>
+        </Switch>
+      )}
+      </Router>
     </div>
   );
 }
